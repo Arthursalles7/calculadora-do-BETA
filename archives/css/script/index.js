c89bdc1314a6d2ef06bc9b16e608652ac2329c5a
Arthur Salles <arthursalles3012@gmail.com>
2026-01-07 10:31:06 -0300

    teste

---
+++ archives/css/script/index.js
@@ -0,0 +1,8 @@
+// Apenas um aviso
+alert("Olá! Isso é um pop-up simples.");
+
+// Uma pergunta de sim ou não (retorna true ou false)
+const confirmacao = confirm("Deseja excluir este item?");
+
+// Uma caixa para o usuário digitar algo (retorna o texto ou null)
+const nome = prompt("Qual é o seu nome?");
