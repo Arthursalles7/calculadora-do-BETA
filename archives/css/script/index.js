996ac7bf300a9a88335b2619bc5c831036ef6409
Arthur Salles <arthursalles3012@gmail.com>
2026-01-07 11:27:08 -0300

    calculadora formada

--- archives/css/script/index.js
+++ archives/css/script/index.js
@@ -1,8 +1,2 @@
-// Apenas um aviso
-alert("Olá! Isso é um pop-up simples.");
+alert("teste");
 
-// Uma pergunta de sim ou não (retorna true ou false)
-const confirmacao = confirm("Deseja excluir este item?");
-
-// Uma caixa para o usuário digitar algo (retorna o texto ou null)
-const nome = prompt("Qual é o seu nome?");
