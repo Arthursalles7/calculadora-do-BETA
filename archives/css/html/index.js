42c3890d9ff792e158a220c63c417dd0b6a6062f
Arthur Salles <arthursalles3012@gmail.com>
2026-01-07 16:42:39 -0300

    finalizando

--- archives/css/html/index.js
+++ archives/css/html/index.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Opcional: Faz o visor voltar ao "0" depois de 2.5 segundos
             setTimeout(() => {
-                if (visor.value === "Não sobra nada pro beta!!") {
+                if (visor.value === "Não sobra nada pro beta!!!!!") {
                     visor.value = "0";
                 }
             }, 2500);
